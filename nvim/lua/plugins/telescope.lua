5b4d5ba439d490d177f2a91bdcc4883911a8aae3
Maksym Rusanov <1maksymrusanov1@gmail.com>
2025-11-15 15:21:04 +0000

    change nvim config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,43 +1,24 @@
 return {
   {
-    "nvim-telescope/telescope.nvim",
-    lazy = false,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    cmd = "Telescope",
-    keys = {
-      { "<C-p>", "<CMD>Telescope find_files<CR>",  mode = { "n", "v" }, desc = "Telescope find file" },
-      { "<Leader>fg", "<CMD>Telescope live_grep<CR>",   mode = { "n", "v" }, desc = "Telescope live grep" },
-      { "<Leader>fc", "<CMD>Telescope commands<CR>",    mode = { "n", "v" }, desc = "Telescope commands" },
-      { "<Leader>fk", "<CMD>Telescope keymaps<CR>",     mode = { "n", "v" }, desc = "Telescope keymaps" },
-      { "<Leader>fb", "<CMD>Telescope buffers<CR>",     mode = { "n", "v" }, desc = "telescope bugffers" },
-      { "<Leader>xx", "<CMD>Telescope diagnostics<CR>", mode = { "n", "v" }, desc = "telescope diagnostics" },
-      { "<Leader>b", "<CMD>Telescope buffers<CR>", mode = { "n" }, desc = "bufferline pick" },
-
-    },
-    config = true,
+    "nvim-telescope/telescope-ui-select.nvim",
   },
   {
-    "nvim-telescope/telescope-ui-select.nvim",
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      local telescope = require("telescope")
-      telescope.setup({
-        defaults = {
-            layout_strategy = "vertical",
-            layout_config = { height=1, width=1 },
-        },
-        vertical = {
-          width = 0.1
-        },
+      require("telescope").setup({
         extensions = {
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown({
-              -- even more opts
-            }),
+            require("telescope.themes").get_dropdown({}),
           },
         },
       })
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+
       require("telescope").load_extension("ui-select")
     end,
   },
