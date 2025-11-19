058d63ce7c9fe237138df3a3fbea3b36dafca987
maksym <1maksymrusanov1@gmail.com>
2025-11-19 21:54:00 +0000

    change nvim  config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -9,8 +9,8 @@ vim.keymap.set({ "n", "v" }, "<A-]>", "<cmd>bn<CR>")
 vim.keymap.set({ "n", "v", "i" }, "<Leader><F10>", "<cmd>qall!<CR>")
 
 -- yank my buffer up
-vim.keymap.set({ "v" }, "<Leader>y", '"+y')  -- E.g: <leader>yy will yank current line to os clipboard
-vim.keymap.set({ "v" }, "<Leader>Y", '"+y$') -- E.g: <leader>yy will yank current line to os clipboard
+vim.keymap.set({ "v" }, "Y", '"+y')  -- E.g: <leader>yy will yank current line to os clipboard
+vim.keymap.set({ "v" }, "<C-Y>:", '"+y$') -- E.g: <leader>yy will yank current line to os clipboard
 vim.keymap.set({ "n" }, "<Leader>p", '"+p')  -- Paste after cursor from clipboard
 vim.keymap.set({ "n" }, "<Leader>P", '"+P')  -- Paste before cursor from clipboard
 -- Disable arrow keys in normal, insert, and visual modes
--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -4,6 +4,15 @@ return {
   event = { "BufReadPost", "BufNewFile" },
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
-    require("bufferline").setup()
+    require("bufferline").setup({
+      options = {
+        mode = "tabs",
+      },
+      keys = {
+        { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+        { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+      },
+    })
   end,
 }
+
