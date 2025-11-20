c98ddf4f89dced89e0db6f2168f29619029dc102
maksym <1maksymrusanov1@gmail.com>
2025-11-20 15:54:29 +0000

    change nvim conf

--- nvim/lua/plugins/blank-line.lua
+++ nvim/lua/plugins/blank-line.lua
@@ -2,7 +2,6 @@ return{
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
 	opts = function(_, opts)
-		-- Other blankline configuration here
 		return require("indent-rainbowline").make_opts(opts)
 	end,
 	dependencies = {
