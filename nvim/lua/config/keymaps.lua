7c0cdae479fa3a2eb5df7eafd065618f2bca8616
maksym <1maksymrusanov1@gmail.com>
2025-11-24 19:32:50 +0000

    change nvim config(add shortcut to center line at middle when press G)

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -9,16 +9,17 @@ vim.keymap.set({ "n", "v" }, "<A-]>", "<cmd>bn<CR>")
 vim.keymap.set({ "n", "v", "i" }, "<Leader><F10>", "<cmd>qall!<CR>")
 
 -- yank my buffer up
-vim.keymap.set({ "v" }, "Y", '"+y')  -- E.g: <leader>yy will yank current line to os clipboard
+vim.keymap.set({ "v" }, "Y", '"+y') -- E.g: <leader>yy will yank current line to os clipboard
 vim.keymap.set({ "v" }, "<C-Y>:", '"+y$') -- E.g: <leader>yy will yank current line to os clipboard
-vim.keymap.set({ "n" }, "<Leader>p", '"+p')  -- Paste after cursor from clipboard
-vim.keymap.set({ "n" }, "<Leader>P", '"+P')  -- Paste before cursor from clipboard
+vim.keymap.set({ "n" }, "<Leader>p", '"+p') -- Paste after cursor from clipboard
+vim.keymap.set({ "n" }, "<Leader>P", '"+P') -- Paste before cursor from clipboard
 -- Disable arrow keys in normal, insert, and visual modes
-vim.keymap.set({'n'}, '<Up>', '<Nop>')
-vim.keymap.set({'n'}, '<Down>', '<Nop>')
-vim.keymap.set({'n'}, '<Left>', '<Nop>')
-vim.keymap.set({'n'}, '<Right>', '<Nop>')
+vim.keymap.set({ "n" }, "<Up>", "<Nop>")
+vim.keymap.set({ "n" }, "<Down>", "<Nop>")
+vim.keymap.set({ "n" }, "<Left>", "<Nop>")
+vim.keymap.set({ "n" }, "<Right>", "<Nop>")
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up" })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down" }) 
-vim.keymap.set({'n','v','i'},'<C-s>','<cmd>%w<CR>')
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down" })
+vim.keymap.set({ "n", "v", "i" }, "<C-s>", "<cmd>%w<CR>")
 vim.keymap.set("i", "<C-s>", "<Esc><cmd>%w<CR>")
+vim.keymap.set("n", "G", "Gzz", { desc = "Center line on middle" })
