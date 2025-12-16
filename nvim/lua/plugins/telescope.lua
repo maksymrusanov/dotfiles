7d54bea15b7b6939591f7e37359c9eaa9fcf463d
maksym <1maksymrusanov1@gmail.com>
2025-12-16 10:59:32 +0000

    change nvim config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -18,8 +18,7 @@ return {
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-
-      require("telescope").load_extension("ui-select")
+      vim.keymap.set('n', '<leader>td', builtin.diagnostics, { desc = 'Telescope diagnostics' })
     end,
   },
 }
